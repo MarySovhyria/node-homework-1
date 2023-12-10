737027555d9b735693ee25326f8195a44cda184b
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2023-12-10 01:00:16 +0000

    first commit

---
+++ contacts.js
@@ -0,0 +1,39 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from 'nanoid';
+const contactsPath = path.resolve("db", "contacts.json");
+
+export async function listContacts() {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+}
+
+export async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const result = contacts.find(item => item.id === contactId);
+    return result || null;
+}
+
+export async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const indexToRemove = contacts.findIndex(item => item.id === contactId);
+    if (indexToRemove == -1) {
+       return null;
+    }
+    const [result] = contacts.splice(indexToRemove, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+}
+
+export async function addContact(name, email, phone) {
+    const contacts = await listContacts(); 
+    const newContacts = {
+        id: nanoid(),
+        name,
+        email,
+        phone
+    }
+    contacts.push(newContacts);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContacts;
+}
